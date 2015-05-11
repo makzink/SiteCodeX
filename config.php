fa6dac779aba4b82d38e90e07cec873cb34f5b8d
MoonBlade <moonblade168@gmail.com>
2015-05-11 12:31:43 +0530

    responsive

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $currency = '$'; //Currency sumbol or code
-$db_username = 'root';
-$db_password = 'nizka';
-$db_name = 'kazmikskitchen';
+$db_username = 'moonblade';
+$db_password = 'IDA3dRbTf0hZ';
+$db_name = 'products';
 $db_host = 'localhost';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 ?>
